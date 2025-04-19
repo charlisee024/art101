0bc4bdac8a2012046deaa54a69eb7e28b70eee61
charlisee024 <charlisegonzales10@gmail.com>
2025-04-19 15:30:20 -0700

    wv

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -8,7 +8,20 @@
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
-  // some code here
+  //1. Draw a hanging post<br>
+                        
+	//2. One player picks a word<br>
+	//3. That player draws lines representing the amount of letters in the word<br>
+	//4. The other player guesses a letter<br>
+  //a. If it is in the chosen word:<br>
+  //i. Write the letter in the word<br>
+  //b. If it is not in the chosen word:<br>
+  //i. draw a body part on the hanging post<br>
+	//5. If the hanging post has a complete man on it:<br>
+  //a. The guessing player loses<br>
+	//6. If the word is guessed before there is a man on the hang post:<br>
+  //a. The guessing player wins<br>
+	//7. Reapeat step 4 until the game ends.</p><br></br>// some code here
   // return results;
 }
 
