7ee7b5326d662a9089af4293d6809e6757127bcc
charlisee024 <charlisegonzales10@gmail.com>
2025-04-19 15:54:40 -0700

    skib

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,21 @@
-// index.js - purpose and description here
+// index.js - Hang Man
 // Author: Charlise Gonzales
 // Date:4/19/2025
+
+
+
+  //1. Draw a hanging post                
+	//2. One player inputes a word
+	//3. # of lines genereated based on the input word length
+	//4. The other player inputs a guess letter
+    //a. If input is chosen word:
+      //i. Display the letter in the lines in its relative position within the word
+    //b. Else: not in the chosen word:
+      //i. Display a new body part on the hanging post
+	//5. If the hanging post has a complete man on it:
+    //a. Player 2 loses
+	//6. If the word is guessed before there is a man on the hang post:
+    //a. Player 2 wins
 
 // Constants
 
@@ -8,21 +23,8 @@
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
-  //1. Draw a hanging post<br>
-                        
-	//2. One player picks a word<br>
-	//3. That player draws lines representing the amount of letters in the word<br>
-	//4. The other player guesses a letter<br>
-  //a. If it is in the chosen word:<br>
-  //i. Write the letter in the word<br>
-  //b. If it is not in the chosen word:<br>
-  //i. draw a body part on the hanging post<br>
-	//5. If the hanging post has a complete man on it:<br>
-  //a. The guessing player loses<br>
-	//6. If the word is guessed before there is a man on the hang post:<br>
-  //a. The guessing player wins<br>
-	//7. Reapeat step 4 until the game ends.</p><br></br>// some code here
-  // return results;
+  // function code
+    // return results;
 }
 
 function main() {
