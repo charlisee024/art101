fcd539e18e89ec2cdd407f82c4e44868a38e2e3b
charlisee024 <charlisegonzales10@gmail.com>
2025-05-04 22:39:05 -0700

    hi

---
+++ lab7/js/lab.js
@@ -0,0 +1,30 @@
+// Lab 7: Functions
+// Author: Charlise Gonzales
+// Date: 05/04/25
+
+
+// Constants
+
+// Functions
+
+function sortUserName() {
+    // Prompt the user for their name
+    var userName = window.prompt("What is your name so I can fix it?");
+    console.log("userName =", userName);
+
+    // Convert the name to an array, sort it, then join it back into a string
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+    // sort Array
+
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+    // join array back to string
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+        // Return the sorted name
+    return nameSorted;
+}
+//output
+document.writeln("Your sorted name is: ",
+    sortUserName(), "</br>");
