28cd0c0d821ac98e7c4dec3e76800a158b54694e
charlisee024 <charlisegonzales10@gmail.com>
2025-05-22 14:32:19 -0700

    bye

---
+++ lab12/js/lab.js
@@ -0,0 +1,58 @@
+// index.js - purpose and description here
+// Author: Charlise Gonzales
+// Date: 5/22/25
+
+
+
+
+ 
+
+
+// Constants
+
+// Functions
+
+function sortingHat(str){
+    let length = str.length;
+    let mod = length % 4;
+
+// return a house depending on the mod result
+    if (mod === 0) {
+        return "Gryffindor";
+    } else if (mod === 1) {
+        return "Ravenclaw";
+    } else if (mod === 2) {
+        return "Hufflepuff";
+    } else if (mod === 3) {
+        return "Slytherin";
+    }
+}
+
+// main function to run when page is ready
+function main() {
+    console.log("Main function started.");
+
+
+    // click listener for button
+    $("#button").click(function() {
+        let name = $("#input").val();
+        let house = sortingHat(name);
+        $("#output").append(
+            <p>The Sorting Hat has sorted you into <strong>${house}</strong>!</p>
+        );
+    });
+}
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+    // some code here
+    // return results;
+  }
+  
+  function main() {
+    console.log("Main function started.");
+    // the code that makes everything happen
+  }
+  
+  // let's get this party started
+  main();
